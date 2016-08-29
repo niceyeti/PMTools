c9f14a336aa00ba053fa4a940f8c69c330f8f733
Jesse Waite <jesse_waite@hotmail.com>
2016-08-28 18:43:43 -0700

    fixed the inductive miner to include all traces, using setNoiseThreshold(0)

--- PromTools/inductiveTemplate.js
+++ PromTools/inductiveTemplate.js
@@ -3,6 +3,9 @@ The template miner script. This isn't actual javascript, just the java-style scr
 that is parsed by the Prom66 cli. Note the $1, $2, $3 anchors; this file is read, the anchors
 are swapped with some parameters, and a new script can thereby be generated on the
 fly from this template.
+
+See these https://svn.win.tue.nl/trac/prom/browser/Packages/InductiveMiner/Trunk/src/org/processmining/plugins/InductiveMiner/mining/MiningParameters.java?rev=22217
+https://svn.win.tue.nl/trac/prom/browser/Packages/InductiveMiner/Trunk/src/org/processmining/plugins/InductiveMiner/mining/MiningParametersIMi.java?rev=22217
 */
 
 System.out.println("Loading log from $1");
@@ -11,6 +14,9 @@ log = open_xes_log_file("$1");
 System.out.println("Creating settings");
 org.processmining.plugins.InductiveMiner.mining.MiningParametersIMi parameters = new org.processmining.plugins.InductiveMiner.mining.MiningParametersIMi();
 //org.deckfour.xes.classification.XEventClassifier classifier = new org.deckfour.xes.classification.XEventAttributeClassifier("Classifier name", "$2");
+
+//Set noise threshold to 0.0. This is CRITICAL to make the InductiveMiner include all traces in its model.
+parameters.setNoiseThreshold(0);
 
 System.out.println("Calling miner");
 pn_and_marking = mine_petri_net_with_inductive_miner_with_parameters(log, parameters);
